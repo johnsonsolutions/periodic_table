bb902dadc410c9a5361b2d78a9590526f8efa6d1
Tevin Johnson <johnsontevin@live.com>
2025-04-03 08:22:59 +0000

    fix: almost..

--- element.sh
+++ element.sh
@@ -11,11 +11,31 @@ NUMCALL() {
   TYP=$($PSQL "select type from types inner join properties using(type_id) where atomic_number='$NUM';")
   echo "The element with atomic number $NUM is $NAM ($SYM). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
 }
-#SYMCALL() {}
-#NAMCALL() {}
+NAMCALL() {
+  NAM=$1
+  NUM=$($PSQL "select atomic_number from elements where name = '$NAM'")
+  SYM=$($PSQL "select symbol from elements where atomic_number = '$NUM'")
+  AMASS=$($PSQL "select atomic_mass from properties where atomic_number = '$NUM'")
+  MPC=$($PSQL "select melting_point_celsius from properties where atomic_number = '$NUM'")
+  BPC=$($PSQL "select boiling_point_celsius from properties where atomic_number = '$NUM'")
+  TYP=$($PSQL "select type from types inner join properties using(type_id) where atomic_number='$NUM';")
+  echo "The element with atomic number $NUM is $NAM ($SYM). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
+
+}
+SYMCALL() {
+  SYM=$1
+  NUM=$($PSQL "select atomic_number from elements where symbol = '$SYM'")
+  NAM=$($PSQL "select name from elements where atomic_number = '$NUM'")
+  AMASS=$($PSQL "select atomic_mass from properties where atomic_number = '$NUM'")
+  MPC=$($PSQL "select melting_point_celsius from properties where atomic_number = '$NUM'")
+  BPC=$($PSQL "select boiling_point_celsius from properties where atomic_number = '$NUM'")
+  TYP=$($PSQL "select type from types inner join properties using(type_id) where atomic_number='$NUM';")
+  echo "The element with atomic number $NUM is $NAM ($SYM). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
+}
+
 SPLIT() {
-  #echo "bird is good $1"
-  NUMCALL $1
+  #judgement
+  SYMCALL $1
 }
 
 
